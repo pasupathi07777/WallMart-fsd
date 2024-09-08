2e303c22490ad4aa6599c9047b003235292ebd64
pasupathi07777 <pasupathi07777@gamil.com>
2024-09-08 18:56:22 +0530

    2.2

--- frontend/src/component/footer/Footer.js
+++ frontend/src/component/footer/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='p-2 text-center capitalize  bg-[#232F3E]  text-white'>WallMart - 2023-2024, All Rights Reserved</div>
+    <div className='p-2 text-center capitalize  bg-[#232F3E]  text-white'> &copy; 2023-2024 WallMart. All Rights Reserved.</div>
   )
 }
 
 export default Footer
+
+
+
